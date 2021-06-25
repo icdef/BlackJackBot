27ec8b6d4afb91845ca5ad9ad125ba2740ad7171
Kian <e01529142@student.tuwien.ac.at>
2021-06-25 02:47:56 +0200

    improved clear command

--- src/main/java/main/non_blackjack_commands/ClearCommand.java
+++ src/main/java/main/non_blackjack_commands/ClearCommand.java
@@ -1,8 +1,10 @@
 package main.non_blackjack_commands;
 ;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.entities.TextChannel;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ClearCommand {
@@ -10,15 +12,15 @@ public class ClearCommand {
 
    public void clearChannel (String input, TextChannel channel){
        if (input.equals("clear")) {
+           List<Message> channelMessages;
            MessageHistory history = new MessageHistory(channel);
            channel.sendMessage("Bot is working, please wait...").complete();
-           history.retrievePast(100).complete();
-           while (history.getRetrievedHistory().size() > 1) {
-               channel.deleteMessages(history.getRetrievedHistory()).complete();
-               history = new MessageHistory(channel);
-               history.retrievePast(100).complete();
+           do {
+               channelMessages = history.retrievePast(100).complete();
+               channel.purgeMessages(channelMessages);
            }
-           channel.sendMessage("Messages deleted. Thanks for waiting")
+           while (!channelMessages.isEmpty());
+           channel.sendMessage("Task done. Thanks for waiting")
                    .queueAfter(1, TimeUnit.SECONDS, m -> m.delete().queueAfter(500, TimeUnit.MILLISECONDS));
        }
 
